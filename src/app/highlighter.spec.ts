0c3958ecbd61cce40cd8c76441f229e4a909f422
bobbylight <robert.e.futrell@gmail.com>
2024-06-16 11:09:47 -0400

    Migrating from jest to vitest

--- src/app/highlighter.spec.ts
+++ src/app/highlighter.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from 'vitest';
 import highlighter from './highlighter';
 import JsonParser from './parsers/json-parser';
 
--- src/app/parsers/json-parser.spec.ts
+++ src/app/parsers/json-parser.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from 'vitest';
 import JsonParser from './json-parser';
 import { Token, TokenType } from '../index';
 
