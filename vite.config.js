146ea0149d6d67a82f40f2391f13562edd062434
bobbylight <robert.e.futrell@gmail.com>
2024-06-16 12:01:34 -0400

    Attempting to fix coverage and GitHub CI

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,10 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    test: {
+        coverage: {
+            include: ['src/app/**/*.ts'],
+        },
+    },
 })
 
